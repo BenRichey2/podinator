2c5e65435a6e6ecbeb0f31f5588fd95f377e8478
Ben Richey <b.richeypg@gmail.com>
2023-11-21 22:13:23 -0500

    tmp

--- src/gradio-demo.py
+++ src/gradio-demo.py
@@ -40,25 +40,45 @@
 #if __name__ == "__main__":
 #    demo.launch(show_api=False)
 #
+#
+#import gradio as gr
+#import numpy as np
+#import time
+#
+## define core fn, which returns a generator {steps} times before returning the image
+#def fake_diffusion(steps):
+#    for _ in range(int(steps)):
+#        time.sleep(1)
+#        image = np.random.random((600, 600, 3))
+#        yield image
+#    image = np.ones((1000,1000,3), np.uint8)
+#    image[:] = [255, 124, 0]
+#    yield image
+#
+#
+#demo = gr.Interface(
+#    fake_diffusion,
+#    inputs=gr.Slider(1, 10, 3, step=1.0),
+#    outputs="image"
+#)
+#
+## define queue - required for generators
+#demo.queue()
+#
+#demo.launch()
 
 import gradio as gr
-import numpy as np
-import time
 
-# define core fn, which returns a generator {steps} times before returning the image
-def fake_diffusion(steps):
-    for _ in range(int(steps)):
-        time.sleep(1)
-        image = np.random.random((600, 600, 3))
-        yield image
-    image = np.ones((1000,1000,3), np.uint8)
-    image[:] = [255, 124, 0]
-    yield image
+def greet(name):
+    return "Hello " + name + "!"
 
-
-demo = gr.Interface(fake_diffusion, inputs=gr.Slider(1, 10, 3), outputs="image")
-
-# define queue - required for generators
-demo.queue()
-
-demo.launch()
+with gr.Blocks() as demo:
+    name = gr.Textbox(label="Name")
+    output = gr.Textbox(label="Output box")
+    greet_btn = gr.Button("Greet")
+    greet_btn.click(
+        fn=greet,
+        inputs=name,
+        outputs=output,
+        api_name="greet"
+    )
